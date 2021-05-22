b3d7851a9b2523dc5139ba36a8915ebfcc800abe
Madara <madara_kruzina@inbox.lv>
2021-05-23 00:22:40 +0300

    updates

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request
 import csv
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-with open('./static/data.csv', mode='w', newline='') as csv_file:
+with open('./static/data.csv', mode='a+', newline='') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames=['name', 'surname'])
     csv_writer.writeheader()
 
@@ -16,7 +15,6 @@ def hello_world():
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    # return 'jauna lapa'
     if request.method == 'POST':
         name = request.form['name']
         surname = request.form['surname']
@@ -26,21 +24,46 @@ def main():
     return render_template('main.html')
 
 
-@app.route('/get_names', methods=['POST'])
+@app.route('/get_names', methods=['GET', 'POST'])
 def get_names():
-    is_valid = False
-    if 'username' in request.json:
-        if request.json['username'] == 'Madara':
-            is_valid = True
     with open('./static/data.csv') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         data = {}
         for idx, row in enumerate(csv_reader):
             data[idx] = row
-    return data if is_valid else {}
+    return data
+
+#ja nebūs ievadīts Mara, tad nedos datus, bet atgriezīs tikai {}
+
+    #is_valid = False
+    #if 'username' in request.json:
+        #if request.json['username'] == 'Mara':
+            #is_valid = True
+    #with open('./static/data.csv') as csv_file:
+        #csv_reader = csv.DictReader(csv_file)
+        #data = {}
+        #for idx, row in enumerate(csv_reader):
+            #data[idx] = row
+        #return data if is_valid else {}
+
+@app.route('/get_count', methods=['POST'])
+def get_count():
+    is_valid = False
+    if 'key' in request.json:
+        if request.json['key'] == 1234:
+            is_valid = True
+    data = str(len(get_names()))
+    return data if is_valid else {'error': 'get the right key'}
+
+    #key = request.form['key']
+    #if key != 'password':
+        #return 'error: get the right key'
+    #data = str(len(get_names()))
+    #return data
 
 
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
 if __name__ == '__main__':
     app.run()
-
-
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-import requests
+#import requests
 
-url = 'http://127.0.0.1:5000/get_names'
+#url = 'http://127.0.0.1:5000/get_names'
+#payload = {'username': 'Madara'}
+#names = requests.post(url, json=payload)
 
-payload = {'username': 'Madara'}
-names = requests.post(url, json=payload)
-print(names.json())
+#names - requests.get(url)
+
+#print(names.json())
