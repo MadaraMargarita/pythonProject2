5e0516d8b948a3dbaaaf567d3ee8726107e2513d
Madara <madara_kruzina@inbox.lv>
2021-05-30 23:18:25 +0300

    updates - 13_Mājasdarbs

--- static/scripts/jquery_testing.js
+++ static/scripts/jquery_testing.js
@@ -1,39 +1,54 @@
 $(document).ready(function() {
     console.log("ready!");
 
-    $("#box").click(function(){
-        /* $("#tekstins").hide() */
-        $("#tekstins").toggle(); /* uzspiež pazūd, uzspiež parādās */
-        /* $("h1").hide(2000).delay(1000).show(2000); */
+    //$("#box").click(function(){
+        //$("#tekstins").hide()
+        //$("#tekstins").toggle(); /* uzspiež pazūd, uzspiež parādās */
+        // $("h1").hide(2000).delay(1000).show(2000);
+    //});
+
+    $("#box").mouseenter(function(){
+        $(this).css('background', 'red');
     });
 
-    $("#box").hover(function(){
-        $("#tekstins").toggle(); /* uzspiež pazūd, uzspiež parādās */
-
-        var width = $(this).width();
-
-        $(this).css("width", width + 100);
-        console.log(width);
+    $("#box").mouseleave(function(){
+        $(this).css('background', 'yellow');
     });
+
+    $("#box").dblclick(function(){
+        $(this).css('background', 'green');
+    });
+
+    //$("#box").hover(function(){
+        //$("#tekstins").toggle(); /* uzspiež pazūd, uzspiež parādās */
+
+        //var width = $(this).width();
+
+        //$(this).css("width", width + 100);
+        //console.log(width);
+    //});
+
 
     //callback funkc.
-    $(document).keypress(function(event) {
-        console.log(event.charCode);
+    //$(document).keypress(function(event) {
+        //console.log(event.charCode);
 
-        var key = String.fromCharCode(event.charCode);
-        console.log(key);
+        //var key = String.fromCharCode(event.charCode);
+        //console.log(key);
 
-        var width = $("#box").width();
-        if(key == 'a'){
-            $("#box").css("width", width + 5);
-        }
+        //var width = $("#box").width();
+        //if(key == 'a'){
+            //$("#box").css("width", width + 5);
+        //}
 
-        if(key == 'd'){
-            $("#box").css("width", width - 5);
-        }
-    });
+        //if(key == 'd'){
+            //$("#box").css("width", width - 5);
 
-    $("h1").hide(2000).delay(1000).show(2000);
+        //}
+    //});
+
+    //$("h1").hide(2000).delay(1000).show(2000);
+    $("h1").hide(100).show(2000);
 
    //Ajax
    //setInterval(function(){
